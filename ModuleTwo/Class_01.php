6b744df82693589c4d52686f38fd4d89a4b4dea4
JoamirS <jmrsilveira@outlook.com>
2020-10-28 09:41:06 -0300

    Array

---
+++ ModuleTwo/Class_01.php
@@ -0,0 +1,7 @@
+<?php
+
+$ageList = [21, 19, 13, 25, 30, 41];
+
+$oneAge = $ageList[2];
+
+echo $oneAge;
---
+++ ModuleTwo/Class_02.php
@@ -0,0 +1,7 @@
+<?php
+
+$ageList = [21, 19, 13, 25, 30, 41];
+
+for ($i = 0; $i < count($ageList); $i++) {
+    echo $ageList[$i].PHP_EOL;
+}
